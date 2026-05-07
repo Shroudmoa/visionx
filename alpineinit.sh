688b7a1bb5422119df78b97dfe7c847141e73bbf
Shroudmoa <mikeshroud10@gmail.com>
2026-05-07 10:19:25 +0200

    Add Alpine setup script for environment initialization
    This script sets up an Alpine Linux environment by updating the package manager, installing necessary packages, configuring doas, and creating a user.

---
+++ alpineinit.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+set -e
+
+echo "[+] Starte Alpine Setup..."
+
+if [ "$(id -u)" -ne 0 ]; then
+  echo "Bitte als root ausführen (su -)"
+  exit 1
+fi
+
+apk update
+apk add doas
+
+# Repos
+cat > /etc/apk/repositories <<EOF
+http://dl-cdn.alpinelinux.org/alpine/edge/main
+http://dl-cdn.alpinelinux.org/alpine/edge/community
+EOF
+
+apk update
+doas apk add vim nano iptables strongswan fastfetch make cmake ninja gcc g++
+
+# doas config
+cat > /etc/doas.conf <<EOF
+permit persist :wheel
+EOF
+
+# user
+adduser -D vm
+adduser vm wheel
